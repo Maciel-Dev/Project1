6470b015a5ef140cfb7658a8d8d7947c4593352b
Maciel-dev <jvmacielv@gmail.com>
2021-08-06 08:22:10 -0300

    A

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.http import response
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import markdown
 from markdown.core import Markdown
 from django.urls import reverse
@@ -65,8 +65,10 @@ def newPage(request):
 
         if title in util.list_entries():
 
-            return render(request, "encyclopedia/pageAlreadyExists.html", {
-                "titleExist": title
+            msg = f"Page with {title} already Created! Please, choose another title!"
+
+            return render(request, "encyclopedia/newPage.html", {
+                "error": msg
         })
 
         else:
@@ -80,8 +82,10 @@ def newPage(request):
             })
 
     if title in util.list_entries():
-        return render(request, "encyclopedia/pageAlreadyExists.html", {
-            #Criar alert
+        msg = "Mensagem de erro!"
+
+        return render(request, "encyclopedia/newPage.html", {
+            "error": msg
         })
 
     return render(request, "encyclopedia/newPage.html")
@@ -89,27 +93,45 @@ def newPage(request):
 
 def pageEdit(request, namePage):
 
-    pageFile = open(f'entries\{namePage}.md', 'r+')
-    reader = pageFile.read()
-    pageFile.truncate(0)
-    pageFile.close()
-    pageFileAppend = open(f'entries\{namePage}.md', 'a', encoding="utf-8")
-    content = request.GET.get('Body', '')
-    appendThis = content
-    pageFileAppend.write(appendThis)
+    reader = str("")
 
-    return render(request, "encyclopedia/pageEditTeste.html", {
+    if request.method == "GET":
+        pageFile = open(f'entries/{namePage}.md', 'r+')
+        reader = pageFile.read()
+        pageFile.truncate(0)
+        pageFile.close()
+        pageFileWrite = open(f'entries/{namePage}.md', 'w')
+        pageFileWrite.write(reader)
+        pageFileWrite.close()
+    
+    elif request.method == "POST":
+        #Talvez use reverse
+        pageFile = open(f'entries/{namePage}.md', 'r+')
+        reader = pageFile.read()
+        pageFile.truncate(0)
+
+        pageFileAppend = open(f'entries\{namePage}.md', 'a', encoding="utf-8")
+        
+        if request.POST.get('Body'):
+            content = request.POST.get('Body')
+            pageFileAppend.write(content)
+            
+            return HttpResponseRedirect(f"/wiki/{namePage}")
+
+
+    return render(request, 'encyclopedia/pageEditTeste.html', {
         "textBody": reader,
         "page": namePage
     })
 
+
 def randomPage(request):
     lista = util.list_entries()
     pageRandom = random.choice(lista)
-    mark = markdown.Markdown()
     page = util.get_entry(pageRandom)
+    mark = markdown.Markdown()
 
-    return render(request, "encyclopedia/pages.html", {
+    return render(request, "encyclopedia/randomPage.html", {
         "names": mark.convert(page),
         "title": pageRandom
         
